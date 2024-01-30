f73b917773ddec0afaba3f1d4d2c8eb99382d655
tieptv <tiepnguyenvantv97@gmail.com>
2024-01-30 20:20:52 +0700

    Create dev branch

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ FROM openjdk:8-jdk-alpine
 
 COPY --from=build /opt/app/target/*.jar app.jar
 
-ENV PORT 8081
+ENV PORT 8096
 EXPOSE $PORT
 
 ENTRYPOINT ["java","-jar","-Xmx1024M","-Dserver.port=${PORT}","app.jar"]
--- src/main/java/com/example/excel_service/ExcelServiceApplication.java
+++ src/main/java/com/example/excel_service/ExcelServiceApplication.java
@@ -1,11 +1,11 @@
 package com.example.excel_service;
 
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@OpenAPIDefinition(servers = {@Server(url = "https://9589-42-116-243-247.ap.ngrok.io", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(description = "Default Server URL")})
 @SpringBootApplication
 public class ExcelServiceApplication {
 
